65088a579f50ab7fcea6b3529d4226cf244c7b57
Sadam Khan <sadamkhanccr@hotmail.com>
2021-08-28 13:57:55 +0500

    Drawer updated

--- lib/loggedInScreen.dart
+++ lib/loggedInScreen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/scheduler.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/scheduler.dart';
 
-import 'package:phone_verification/loginScreen.dart';
+// import 'package:phone_verification/loginScreen.dart';
 import 'package:phone_verification/main_drawer.dart';
 
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-var userName = '';
+// final FirebaseAuth _auth = FirebaseAuth.instance;
+// final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+// var userName = '';
 
 class LoggedInScreen extends StatefulWidget {
   LoggedInScreen({Key key}) : super(key: key);
@@ -19,70 +19,53 @@ class LoggedInScreen extends StatefulWidget {
 
 class _LoggedInScreenState extends State<LoggedInScreen> {
   @override
-  void initState() {
-    getUser();
-    super.initState();
-  }
+  // void initState() {
+  //   getUser();
+  //   super.initState();
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Chalo")),
+      appBar: AppBar(
+        title: Center(child: Text("Chalo")),
+      ),
+      drawer: MainDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [Center(child: Text("You are on a homepage."))],
         ),
-        drawer: MainDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Text(
-                'Welcome ' + userName,
-                style: TextStyle(fontSize: 30),
-              ),
-              Text('(cellnumber: ' +
-                  (_auth.currentUser.phoneNumber != null
-                      ? _auth.currentUser.phoneNumber
-                      : '') +
-                  ' uid:' +
-                  (_auth.currentUser.uid != null ? _auth.currentUser.uid : '') +
-                  ')'),
-              ElevatedButton(
-                  onPressed: () => {
-                        //sign out
-                        signOut()
-                      },
-                  child: Text('Sign out'))
-            ],
-          ),
-        ));
+      ),
+    );
   }
 
-  Future getUser() async {
-    if (_auth.currentUser != null) {
-      var cellNumber = _auth.currentUser.phoneNumber;
-      cellNumber =
-          '0' + _auth.currentUser.phoneNumber.substring(3, cellNumber.length);
-      debugPrint(cellNumber);
-      await _firestore
-          .collection('users')
-          .where('cellnumber', isEqualTo: cellNumber)
-          .get()
-          .then((result) {
-        if (result.docs.length > 0) {
-          setState(() {
-            userName = result.docs[0].data()['name'];
-          });
-        }
-      });
-    }
-  }
+  // Future getUser() async {
+  //   if (_auth.currentUser != null) {
+  //     var cellNumber = _auth.currentUser.phoneNumber;
+  //     cellNumber =
+  //         '0' + _auth.currentUser.phoneNumber.substring(3, cellNumber.length);
+  //     debugPrint(cellNumber);
+  //     await _firestore
+  //         .collection('users')
+  //         .where('cellnumber', isEqualTo: cellNumber)
+  //         .get()
+  //         .then((result) {
+  //       if (result.docs.length > 0) {
+  //         setState(() {
+  //           userName = result.docs[0].data()['name'];
+  //         });
+  //       }
+  //     });
+  //   }
+  // }
 
-  signOut() {
-    //redirect
-    _auth.signOut().then((value) => Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (BuildContext context) => LoginScreen())));
-  }
+  // signOut() {
+  //   //redirect
+  //   _auth.signOut().then((value) => Navigator.pushReplacement(context,
+  //       MaterialPageRoute(builder: (BuildContext context) => LoginScreen())));
+  // }
 }
--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:phone_verification/loggedInScreen.dart';
+
+import 'loginScreen.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+var userName = '';
 
 class MainDrawer extends StatefulWidget {
   @override
@@ -6,6 +15,11 @@ class MainDrawer extends StatefulWidget {
 }
 
 class _MainDrawerState extends State<MainDrawer> {
+  void initState() {
+    getUser();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -29,13 +43,62 @@ class _MainDrawerState extends State<MainDrawer> {
                             "https://st2.depositphotos.com/1104517/11965/v/600/depositphotos_119659092-stock-illustration-male-avatar-profile-picture-vector.jpg"),
                       ),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      userName,
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ),
+                  Text('Phone: ' +
+                      (_auth.currentUser.phoneNumber != null
+                          ? _auth.currentUser.phoneNumber
+                          : '') +
+                      // ' ID: ' +
+                      // (_auth.currentUser.uid != null
+                      //     ? _auth.currentUser.uid
+                      //     : '') +
+                      ''),
+                  ElevatedButton(
+                    onPressed: () => {
+                      //sign out
+                      signOut()
+                    },
+                    child: Text('Sign out'),
+                  ),
                 ],
               ),
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+
+  signOut() {
+    //redirect
+    _auth.signOut().then((value) => Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (BuildContext context) => LoginScreen())));
+  }
+
+  Future getUser() async {
+    if (_auth.currentUser != null) {
+      var cellNumber = _auth.currentUser.phoneNumber;
+      cellNumber =
+          '0' + _auth.currentUser.phoneNumber.substring(3, cellNumber.length);
+      debugPrint(cellNumber);
+      await _firestore
+          .collection('users')
+          .where('cellnumber', isEqualTo: cellNumber)
+          .get()
+          .then((result) {
+        if (result.docs.length > 0) {
+          setState(() {
+            userName = result.docs[0].data()['name'];
+          });
+        }
+      });
+    }
   }
 }
